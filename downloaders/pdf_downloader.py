440869e8768840d0961278b039e984430f5a4a0d
Zi Xiang <tanzixiang@gmail.com>
2021-08-07 15:31:57 +0800

    Tweak import statement

--- downloaders/pdf_downloader.py
+++ downloaders/pdf_downloader.py
@@ -4,11 +4,11 @@ import os
 import random
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 from utilities import errors
 

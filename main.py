a31c07b29cefeeadf94ac1b4c94919226c68fc49
Zi Xiang <tanzixiang@gmail.com>
2021-08-08 22:02:25 +0800

    Clean up main.py somewhat; delete comment from user_interface.py

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ def main():
     ui = downloaderUI()
     successful_initialisation = ui.get_substack_url()
     if not successful_initialisation:
-        print("We're sorry that something has gone wrong.")
+        print_upon_exit_failure()
         return
     successful_log_in = ui.get_user_credential()
     if not successful_log_in:
-        print("We're sorry that something has gone wrong.")
+        print_upon_exit_failure()
         return
 
     successful_download = ui.get_user_download_choices()
     if successful_download:
-        print("Your article(s) have been downloaded. Thank you for using Substack Archives Downloader. Have a nice day!")
+        print_upon_exist_success()
     else:
-        print("We're sorry that something has gone wrong.")
+        print_upon_exit_failure()
+
+
+def print_upon_exit_failure():
+    print("We're sorry that something has gone wrong.")
+
+
+def print_upon_exist_success():
+    print("Your article(s) have been downloaded. Thank you for using Substack Archives Downloader. Have a nice day!")
 
 
 if __name__ == "__main__":
